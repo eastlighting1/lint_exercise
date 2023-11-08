ba970dd91608f09f0171d0354caa125edb73ec95
Kim Donghyeon <31691750+eastlighting1@users.noreply.github.com>
2023-11-08 11:10:53 +0900

    Add files via upload

--- test1.py
+++ test1.py
@@ -1,7 +1,3 @@
-"""
-Easy Pandas Example
-"""
-
 import numpy as np
 import pandas as pd
 
@@ -10,3 +6,4 @@ score = np.array([[70, 45, 60], [30, 25, 95], [23, 53, 100]])
 df = pd.DataFrame(score, columns=['Math', 'Chemistry', 'Physics'])
 
 print(df)
+ 
